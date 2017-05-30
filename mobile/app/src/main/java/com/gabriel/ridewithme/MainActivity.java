9213b5fe9b6b8630ac1cf8fd60226a8b9e7b6b45
Gabriel Stan HQ <stan.gabriel88@gmail.com>
2017-05-30 03:00:18 +0300

    Added new mobile views + background image

--- mobile/app/src/main/java/com/gabriel/ridewithme/MainActivity.java
+++ mobile/app/src/main/java/com/gabriel/ridewithme/MainActivity.java
@@ -3,6 +3,8 @@ package com.gabriel.ridewithme;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText etFullName = (EditText) findViewById(R.id.etFullName);
         final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMsg);
+        final Button bCreateRide= (Button) findViewById(R.id.bCreateRide);
+        final Button bFindRide= (Button) findViewById(R.id.bFindRide);
 
         Intent intent = getIntent();
         String fisrtName = intent.getStringExtra("fistName");
@@ -27,5 +31,20 @@ public class MainActivity extends AppCompatActivity {
         welcomeMessage.setText(message);
         etFullName.setText(fullName);
 
+        bCreateRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createRideIntent = new Intent(MainActivity.this, CreateRideActivity.class);
+                MainActivity.this.startActivity(createRideIntent);
+            }
+        });
+
+        bFindRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent findRideIntent = new Intent(MainActivity.this, FindRideActivity.class);
+                MainActivity.this.startActivity(findRideIntent);
+            }
+        });
     }
 }
